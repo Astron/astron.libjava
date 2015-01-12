9cd91265076ec0ed660f316dba92ebea8e5ac75f
chandler14362 <chandler14362@gmail.com>
2015-01-12 01:29:28 -0500

    dc: Added the KeywordsList

--- src/main/java/astron/dc/DCFile.java
+++ src/main/java/astron/dc/DCFile.java
@@ -8,6 +8,9 @@ import java.util.ArrayList;
 public class DCFile {
 
     private final ArrayList<DImport> _imports = new ArrayList<DImport>();
+    private final KeywordList _keywordList = new KeywordList();
+
+    public DCFile() {}
 
     public void addImport(final DImport dimport) {
         _imports.add(dimport);
@@ -16,6 +19,23 @@ public class DCFile {
     public int getImportCount() {
         return _imports.size();
     }
+
+    public void addKeyword(final String keyword) {
+        _keywordList.addKeyword(keyword);
+    }
+
+    public int getKeywordCount() {
+        return _keywordList.getKeywordCount();
+    }
+
+    public void printInfo() {
+        for (DImport dimport: _imports) {
+            System.out.println(dimport.toString());
+        }
+        System.out.println(_keywordList.toString());
+    }
+
+    // These are just some old methods that we need for the code to compile. This will be fixed soon...
 
     public DClass getDClass(final String name) {
         return new DClass("122121", 2);
@@ -31,12 +51,6 @@ public class DCFile {
 
     public DMethod getDMethod(final int id) {
         return new DMethod("adasds", 2);
-    }
-
-    public void printInfo() {
-        for (DImport dimport: _imports) {
-            System.out.println(dimport.toString());
-        }
     }
 
 }
---
+++ src/main/java/astron/dc/KeywordList.java
@@ -0,0 +1,34 @@
+package astron.dc;
+
+import java.util.ArrayList;
+
+/**
+ * @author chandler14362
+ */
+public class KeywordList {
+
+    private ArrayList<String> _keywords = new ArrayList<String>();
+
+    public KeywordList() {}
+
+    public boolean hasKeyword(final String name) {
+        return _keywords.contains(name);
+    }
+
+    public int getKeywordCount() {
+        return _keywords.size();
+    }
+
+    public String getKeyword(final int index) {
+        return _keywords.get(index);
+    }
+
+    public void addKeyword(final String keyword) {
+        _keywords.add(keyword);
+    }
+
+    public String toString() {
+        return String.format("---KeywordList---\n Keywords: %s", _keywords);
+    }
+
+}
